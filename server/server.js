2c087a6ea528eb33dcd95c0ff0c46aa928ad716d
Andrey Solonikov <andreysol@yahoo.com>
2023-02-13 18:48:47 +0200

    update settings

--- server/server.js
+++ server/server.js
@@ -6,15 +6,25 @@ import { connectDB } from './mongodb/connect.js'
 import { postRoutes } from './routes/postRoutes.js'
 import { solleRoutes } from './routes/solleRoutes.js'
 
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 3030
 
 dotenv.config()
 
 const app = express()
-app.use(cors())
+
 app.use(express.json({ limit: '50mb' }))
 app.use('/api/post', postRoutes)
 app.use('/api/solle', solleRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
+}
 
 app.get('/', async (req, res) => {
   res.send('Hello World!')
